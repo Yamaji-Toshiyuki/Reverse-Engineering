e59e509a0921cd8ca0364b86f8ee9f6381920b6a
haruyuki_16278 <nexus02221toshiyuki@gmail.com>
2019-01-16 17:47:44 +0900

    Arduinoプログラムの改良。 1秒間のみ値を出力するよう設定。

--- signal_recorder/signal_recorder.ino
+++ signal_recorder/signal_recorder.ino
@@ -1,9 +1,43 @@
+void cdown(int times);
+int i,t;
+
 void setup() {
   // put your setup code here, to run once:
+  t = millis();
+  digitalWrite(13,HIGH);
   Serial.begin(9600);
+  pinMode(2,INPUT);
+  pinMode(4,INPUT);
+  pinMode(13,OUTPUT);
+  while(millis() < t+1000){}
+  digitalWrite(13,LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(0));
+  while(digitalRead(2)){
+    cdown(3);
+    t = millis();
+    digitalWrite(13,HIGH);
+    while(millis() < t + 1000){
+      Serial.print(PIND & _BV(4));
+      Serial.print(",");
+      Serial.println(analogRead(0));
+    }
+  Serial.flush();
+  digitalWrite(13,LOW);
+  pinMode(12,OUTPUT);
+  }
+}
+
+void cdown(int times){
+  for(i = 0;i < times;i++){
+    t = millis();
+    digitalWrite(13,HIGH);
+    //Serial.println("HIGH");
+    while(millis() < t+100){}
+    digitalWrite(13,LOW);
+    //Serial.println("LOW");
+    while(millis() < t+500){}
+  }
 }
